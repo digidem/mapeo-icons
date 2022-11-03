7e053f38f5e4194af20947ef32a99c0e5c282e63
Luandro <luandro@digital-democracy.org>
2022-11-03 13:38:40 -0300

    chore: minor ui tweaks

--- components/Search.vue
+++ components/Search.vue
@@ -3,15 +3,20 @@
   <div
     class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
   >
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-        <div class="max-w-lg mx-auto sm:px-1 lg:px-2 mb-5">
-          <img src="/logo.webp">
-        </div>
-        <div class="text-center">
-          <h1 class="font-bold text-4xl mb-5">{{ $t('title') }}</h1>
+    <div class="max-w-4xl mx-auto pt-5vh sm:px-6 lg:px-8">
+      <div class="max-w-lg mx-auto sm:px-1 lg:px-2 mb-5">
+        <img class="mx-auto h-100px md:h-150px" src="/logo.webp" />
+      </div>
+      <div class="text-center">
+        <h1 class="font-bold text-2xl py-4">{{ $t('title') }}</h1>
+        <div class="text-md md:text-lg mx-auto">
           <p class="mb-5">{{ $t('description') }}</p>
-          <p class="mb-5">{{ $t('config') }} <a href="" target="_blank" class="underline">{{ $t('link') }}</a></p>
+          <p class="mb-5">
+            {{ $t('config') }}
+            <a href="" target="_blank" class="underline">{{ $t('link') }}</a>
+          </p>
         </div>
+      </div>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <div class="flex flex-row">
           <h2 class="text-2xl leading-7 font-semibold">{{ $t('welcome') }}</h2>
