04f65ef5d9dd8db9a50fb340735e3836094bffa4
Luandro <luandro@digital-democracy.org>
2022-11-03 09:42:14 -0300

    fix: use big translate instead of google translate

--- api/lib/translate.js
+++ api/lib/translate.js
@@ -1,44 +1,11 @@
-const { translate } = require('@vitalets/google-translate-api')
-
-let tries = 0
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
-}
+const { translate } = require('bing-translate-api')
 
 async function runTranslate(searchTerm, locale) {
-  try {
-    const translation = await translate(searchTerm, { from: locale, to: 'en' })
-    return translation.text
-  } catch (err) {
-    console.log('ERROR on translation', err)
-    await sleep(1000)
-    tries++
-    console.log('Tries', tries)
-    if (tries < 10) runTranslate()
-  }
+  const translation = await translate(searchTerm, locale, 'en', true)
+  console.log('translation', translation)
+  return translation.translation
 }
 
 module.exports = async (searchTerm, locale) => {
-  /* LibreTranslate, doesn't work well */
-  // const { IDIOM } = process.env;
-  // const res = await fetch("https://libretranslate.com/translate", {
-  //   method: "POST",
-  //   body: JSON.stringify({
-  //     q: searchTerm,
-  //     source: IDIOM,
-  //     target: "en",
-  //   }),
-  //   headers: { "Content-Type": "application/json" },
-  // });
-  // const json = await res.json();
-
-  // if (json.error) {
-  //   throw Error(json.error)
-  // }
-  // const text = json.translatedText;
-  // return text;
   return await runTranslate(searchTerm, locale)
 }
